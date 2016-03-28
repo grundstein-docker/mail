5592f9bb6120c2a479a1a4cf3691fe97559f6e4a
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-28 17:34:42 +0200

    add OWNER and NAME

--- script/run.sh
+++ script/run.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-NAME="zixia/simple-mail-forwarder"
+OWNER=${OWNER:-"zixia"}
+NAME=${NAME:-"simple-mail-forwarder"}
+IMAGE_NAME="$OWNER/$NAME"
+
 TAG='' && [ -n "$1" ] && TAG=":$1" && shift
 
-CMD="docker run --detach -e SMF_CONFIG=$SMF_CONFIG ${NAME}${TAG} $@"
+CMD="docker run --detach --name $NAME -e SMF_CONFIG=$SMF_CONFIG $IMAGE_NAME$TAG $@"
 
 echo ">> $CMD"
 $CMD
